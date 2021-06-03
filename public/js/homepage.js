88c60da48be9443e9dd71414a6821a099318b2b0
Iris Valle <iris.valle.design@gmail.com>
2021-06-03 18:50:49 -0400

    updated main hb, homepage hb, css, homepage.js

---
+++ public/js/homepage.js
@@ -0,0 +1,15 @@
+const jawsBtn = document.getElementById('jawsMovie');
+const notebookBtn = document.getElementById('notebookMovie');
+const ozBtn = document.getElementById('wizardOzMovie');
+
+jawsBtn.addEventListener("click", function() {
+    document.location.assign('seatReservation');
+});
+
+notebookBtn.addEventListener("click", function() {
+    document.location.assign('seatReservation');
+});
+
+ozBtn.addEventListener("click", function() {
+    document.location.assign('seatReservation');
+});
